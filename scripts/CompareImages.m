bbd0c0f35fbb4c955fda6d01ac056014374d3a4e
Hegar Garcia <hegargarcia@gmai.com>
2019-02-22 18:59:45 -0600

    Adding image comparaton routine

---
+++ scripts/CompareImages.m
@@ -0,0 +1,19 @@
+function CompareImages(img1, img2) 
+  figure(1);
+  
+  subplot(2, 2, 1);
+  imshow(img1);
+  title('Image');
+
+  subplot(2, 2, 2);
+  imhist(img1);
+  title('Histogram');
+  
+  subplot(2, 2, 3);
+  imshow(img2);
+  title('Image');
+
+  subplot(2, 2, 4);
+  imhist(img2);
+  title('Histogram');
+end
