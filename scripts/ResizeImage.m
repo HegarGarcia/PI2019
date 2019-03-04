64b099c2d74dde086318932b32b84de3e1cf5d0e
Hegar Garcia <hegargarcia@gmail.com>
2019-03-03 20:53:41 -0600

    Saving state

--- scripts/ResizeImage.m
+++ scripts/ResizeImage.m
@@ -1,29 +1,41 @@
-function [outImage] = ResizeImage(img, colorSpace, ratio, layerRange, decompress) 
+function [out] = ResizeImage(img, ratio, layersRange, colorspace) 
   
-  rows = size(img, 1);
-  cols = size(img, 2);
-  outImage = zeros([rows * ratio, cols * ratio, 3]);
+  out = changeColorspace(img, 'rgb', colorspace);
   
-  intRows = ceil([1:(rows * ratio)]./(ratio));
-  intCols = ceil([1:(cols * ratio)]./(ratio));
+  original_rows = size(out, 1);
+  original_cols = size(out, 2);
   
-  for layerNumber = layerRange
-    disp(layerNumber)
-    layer = img(:, :, layerNumber);
-    layer = layer(intRows, :);
-    outImage(:, :, layerNumber) = layer(:, intCols);
+  resized_rows = original_rows * ratio;
+  resized_cols = original_cols * ratio;
+  
+  compressed_layer = compress(out, [resized_rows, resized_cols], layersRange); 
+  decompressed_layer = compress(compressed_layer, [original_rows, original_cols], layersRange);
+  
+  for layerId = layersRange
+    out(:, :, layerId) = decompressed_layer(:, :, layerId);
   end
   
-  if decompress == true
-    invRatio = (floor(rows * ratio) / rows);
-    outImage = ResizeImage(outImage, '', 1 / invRatio, layerRange, false);
-    notCompressedLayers = setdiff(1:3, layerRange);
-    
-    for layerNumber = notCompressedLayers
-      outImage(:, :, layerNumber) = img(:, :, layerNumber);
-    end
-  end
+  out = changeColorspace(out, colorspace, 'rgb');
+end
+
+function [out] = compress(img, sizes, layersRange)
   
-  outImage = uint8(outImage);
+  original_rows = size(img, 1);
+  original_cols = size(img, 2);
+  
+  row_ratio = sizes(1) / original_rows;
+  col_ratio = sizes(2) / original_cols;
+  
+  row_range = 1:(original_rows * row_ratio);
+  col_range = 1:(original_cols * col_ratio);
+  
+  int_rows = ceil(row_range./row_ratio);
+  int_cols = ceil(col_range./col_ratio);
+  
+  for layerId = layersRange
+    layer = img(:, :, layerId);
+    layer = layer(int_rows, :);
+    out(:, :, layerId) = layer(:, int_cols);
+  end
   
 end
